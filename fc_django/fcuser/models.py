5f4c1898ae42edfc60c2de399d886822d0636359
leorld <leorld@naver.com>
2020-08-05 07:10:44 +0900

    form view를 이용한 회원가입, 로그인 작성

--- fc_django/fcuser/models.py
+++ fc_django/fcuser/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Fcuser(models.Model):
+    objects = models.Manager()  # no objects 오류 수정을 위한 추가 코드(없어도됨)
     email = models.EmailField(verbose_name='이메일')
     password = models.CharField(max_length=64, verbose_name='비밀번호')
     register_date = models.DateTimeField(
--- fc_django/fcuser/views.py
+++ fc_django/fcuser/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from .forms import RegisterForm, LoginForm
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html', {'email': request.session.get('user')})
+
+
+class RegisterView(FormView):
+    template_name = 'register.html'
+    form_class = RegisterForm
+    success_url = '/'  # 회원가입 성공시 홈으로 이동
+
+
+class LoginView(FormView):
+    template_name = 'login.html'
+    form_class = LoginForm
+    success_url = '/'
+
+    def form_valid(self, form):  # 세션에 로그인정보 저장
+        self.request.session['user'] = form.email
+        return super().form_valid(form)
